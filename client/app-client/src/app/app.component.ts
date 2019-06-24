eef4ba09a397dacadbf9c8b2b457ff2a3f75f7da
RodrigoOlv <rodrigo.oliveira1998@outlook.com>
2019-06-24 14:54:21 -0300

    eliminando callback hell

--- client/app-client/src/app/app.component.ts
+++ client/app-client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ProductsService } from './products.service';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { filter, switchMap } from 'rxjs/operators';
 import { Product } from './product.model';
 import { MatSnackBar, MatSnackBarConfig, MatDialog } from '@angular/material';
 import { DialogEditProductComponent } from './dialog-edit-product/dialog-edit-product.component';
@@ -20,6 +21,8 @@ export class AppComponent {
   newlyProducts: Product[] = [];
   productsToDelete: Product[];
   productsToEdit: Product[];
+
+  private unsubscribe$: Subject<any> = new Subject();
   
   constructor(
     private productsService: ProductsService,
@@ -30,7 +33,7 @@ export class AppComponent {
   ngOnInit() {}
 
   getSimplesHttpRequest() {
-    this.simpleReqProductObs$ = this.productsService.getProducts();
+    this.productsService.getProducts();
   }
 
   getProductsWithErrorHandling() {
@@ -56,7 +59,8 @@ export class AppComponent {
   }
 
   getProductsWithErrorHandlingOk() {
-    this.productsService.getProductsDelay().subscribe(
+    this.productsService.getProductsDelay()
+    .subscribe(
       (prods) => {
         this.productsErrorHandling = prods;
         
@@ -75,13 +79,13 @@ export class AppComponent {
   getProductsLoading() {
     this.bLoading = true;
 
-    this.productsService.getProductsDelay().subscribe(
+    this.productsService.getProductsDelay()
+    .subscribe(
       (prods) => {
         this.productsLoading = prods;
         this.bLoading = false;
-
       },
-      (err) => {
+      () => {
         this.bLoading = false;
       }
     );
@@ -115,25 +119,6 @@ export class AppComponent {
   saveProduct(name: string, department: string, price: number) {
     let p = {name, department, price};
 
-    this.productsService.saveProduct(p).subscribe(
-      (p: Product) => {
-        console.log(p);
-
-        this.newlyProducts.push(p);
-      },
-      (err) => {
-        console.log(err);
-
-        let config = new MatSnackBarConfig();
-        config.duration = 2000;
-        config.panelClass = ['snack_err'];
-
-        if(err.status == 0)
-          this.snackBar.open('Could not connect to the server', '', config);
-        else
-          this.snackBar.open(err.error.msg, '', config);
-      }
-    );
   }
 
   deleteProduct(p: Product) {
@@ -164,22 +149,21 @@ export class AppComponent {
 
   editProduct(p: Product) {
     let newProduct: Product = {...p};
-
     let dialogRef = this.dialog.open(DialogEditProductComponent, {width: '400px', data: newProduct});
-    
-    dialogRef.afterClosed().subscribe((res: Product) => {
-      // console.log(res);
-      if (res) {
-        this.productsService.editProduct(res).subscribe(
-          (resp) => {
-            let i = this.productsToEdit.findIndex(prod => p._id == prod._id);
 
-            if (i >= 0)
-              this.productsToEdit[i] = resp;
-          },
-          (err) => console.error(err)
-        );
-      }
-    });
+    dialogRef.afterClosed().pipe(
+      filter((prod: Product) => prod != undefined),
+      switchMap((prod: Product) => this.productsService.editProduct(prod))).subscribe(
+      (prod: Product,) => {
+        let i = this.productsToEdit.findIndex(p => p._id == prod._id);
+        if (i>=0)
+          this.productsToEdit[i] = prod;
+      },
+      (err) => console.error(err)
+    );
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
   }
 }
